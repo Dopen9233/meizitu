d0d56090c39d9ce4e0561c8725477197f8a37232
十七號 <923316226@qq.com>
2019-03-14 18:13:20 +0800

    UI界面雏形

--- src/main/java/top/dopen/ui/HomeController.java
+++ src/main/java/top/dopen/ui/HomeController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
@@ -26,6 +27,11 @@ import java.util.ResourceBundle;
 public class HomeController implements Initializable {
     public Map<String ,String > direct = new HashMap<>();
 
+    @FXML
+    private Button sub ;
+
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
@@ -35,19 +41,38 @@ public class HomeController implements Initializable {
     public void getLocalPath(ActionEvent event){
 
     }
+
     public void chooseFilePath(ActionEvent event){
         DirectoryChooser directoryChooser = new DirectoryChooser();
         directoryChooser.setTitle("选择上传路径");
-        directoryChooser.setInitialDirectory(new File("."));
-        /*FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("选择文件夹");
-        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("选择路径", "/");
-        fileChooser.getExtensionFilters().add(extFilter);*/
+        directoryChooser.setInitialDirectory(new File("."));//默认当前路径
         File file = directoryChooser.showDialog(new Stage());
-        /*System.out.println(file);*/
         if(file != null){
             direct.put("localPath",file.getPath());
         }
 
     }
+
+    public void getType(ActionEvent event){
+        System.out.println("修改提交按钮");
+        sub.setDisable(false);
+    }
+
+
+
+    /*public void chooseFilePath(ActionEvent event){
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("选择上传路径");
+        directoryChooser.setInitialDirectory(new File("."));
+        *//*FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("选择文件夹");
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("选择路径", "/");
+        fileChooser.getExtensionFilters().add(extFilter);*//*
+        File file = directoryChooser.showDialog(new Stage());
+        *//*System.out.println(file);*//*
+        if(file != null){
+            direct.put("localPath",file.getPath());
+        }
+
+    }*/
 }
